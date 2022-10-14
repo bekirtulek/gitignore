fe9a7b5ef32f1a0ebb9dd9e849b811100318c265
bekirtulek <brt161822@gmail.com>
2022-10-14 18:54:40 +0300

    22.10.14/18.47

--- src/Gitignore1.java
+++ src/Gitignore1.java
@@ -1,13 +1,5 @@
 public class Gitignore1 {
     public static void main(String[] args) {
-    System.out.println("gitignore deneme 1");
-    System.out.println("gitignore deneme 2 master");
-    System.out.println("gitignore deneme 2 branch ");
-    System.out.println("gitignore deneme 3 branch tahir ");
-    System.out.println("gitignore deneme 3 branch bekir2");
-    System.out.println("gitignore deneme 4 master");
-    System.out.println("gitignore deneme 5 repo");
-    System.out.println("gitignore deneme 6 bekir");
-    System.out.println("gitignore deneme 7 master");
+
 }
 }
