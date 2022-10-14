cd20d7d07994a15ffbd241a97cb148cd1bc85256
bekir <110713701+bekirtulek@users.noreply.github.com>
2022-10-14 19:02:58 +0300

    Update Gitignore1.java

--- src/Gitignore1.java
+++ src/Gitignore1.java
@@ -2,6 +2,7 @@ public class Gitignore1 {
     public static void main(String[] args) {
         System.out.println("1. deneme master");
         System.out.println("2. deneme bekir");
+        System.out.println("2. deneme remote");
 
 }
 }
